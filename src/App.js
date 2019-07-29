f66860fa78da289783a70285edfeb20bdd31c4d6
Eleasah Halsmer <eleasahjh@gmail.com>
2019-07-29 16:10:22 -0600

    Wip: nav. Working on nav.js and added palette to navImages folder

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Nav from './components/nav';
 function App() {
   return (
     <div className="App">
-
+      <Nav />
     </div>
   );
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { Container, Icon, Image, Header } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import palette from '../navImages/palette.png';
 
 function Nav () {
     return(
-        <h1>Navigation</h1>
+        <Container style={navStyle}> {/*add 'fluid' to make so there is no max width (it will grow/shrink to fill its container)*/}
+        {/* <Icon name='edit' size='big'/> */}
+            <Link style={navStyle}>
+                <Image src={palette} height="52.5px" width="52.5px"/>
+                <Header as="h4" textAlign="left" style={{maxWidth:"20px", color: "white", margin: "0px"}}>Art Portfolio</Header>
+            </Link>
+        </Container>
     )
+}
+
+const navStyle={
+    maxWidth: '1500px',
+    height: '80px',
+    // border: '1px solid blue'
+    backgroundColor: '#202E78',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0px 30px 0px 17px'
 }
 
 export default Nav;
