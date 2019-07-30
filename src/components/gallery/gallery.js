b4346a0d679afefa24b4761fb68061177532a2ed
Eleasah Halsmer <eleasahjh@gmail.com>
2019-07-30 13:49:03 -0600

    Making axios call and displaying images. Still need to format sizes, perhaps using semantic ui cards

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import React, { useState, useEffect } from "react";
+import { Grid } from "semantic-ui-react";
+import axios from "axios";
 
-function Gallery(){
-    return (
-        <p>Gallery component to go here</p>
-    )
+function Gallery() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://art-portfolio-be.herokuapp.com/api/images").then(data => {
+      console.log(data);
+      setImages(data.data);
+    });
+  }, []);
+
+  return (
+    <div>
+        {images.map((image)=>{
+            return(
+                <div>
+                    {/* <h3>{post.username}</h3> */}
+                    <img src={image.url}/>
+                </div>
+            )
+        })}
+    </div>
+  )
 }
 
 export default Gallery;
