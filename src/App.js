430b9d1b445a954f011fd860e9a9c7db3cc61524
Eleasah Halsmer <eleasahjh@gmail.com>
2019-07-31 13:22:06 -0600

    Pagination nearly finished on gallery. Just need to add limit to end of array

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <div className="App">
       <NavLoggedIn />
-      {/* <Gallery /> */}
-      <Nav />
-      <CreateAcc/>
-      <Login/>
+      {/* <Nav /> */}
+      <Gallery />
+      {/* <CreateAcc/> */}
+      {/* <Login/> */}
     </div>
   );
 }
--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,31 +1,57 @@
 import React, { useState, useEffect } from "react";
-import { Container, Image, Header, Modal, Pagination } from "semantic-ui-react";
+import {
+  Container,
+  Image,
+  Header,
+  Modal,
+  Button,
+  Icon
+} from "semantic-ui-react";
 import axios from "axios";
 
 function Gallery() {
   const [images, setImages] = useState([]);
-  const [activePage, setActivePage] = useState(1);
+  const [myActivePage, setMyActivePage] = useState(1);
+  const [activeImages, setActiveImages] = useState([]);
 
-//   Function for returning slices of array, based on number of images per page (n), and which page we're on (page).
-  function paginate(array, n, page){
-    if (page === 1){
-        return array.slice(0,n)
+  //   Function for returning slices of array, based on number of images per page (n), and which page we're on (page).
+  function paginate(array, n, page) {
+    if (page === 1) {
+      return array.slice(0, n);
     } else {
-        return array.slice((page-1)*n, page*n)
+      return array.slice((page - 1) * n, page * n);
     }
   }
 
-//   function handlePageChange((e, {activePage})=>)
+  const handlePageChangeRight = e => {
+    console.log("event", e);
+    setMyActivePage(myActivePage + 1);
+    // setActiveImages(paginate(images, 9, myActivePage));
+    console.log("active page after right button:", myActivePage);
+  };
+  const handlePageChangeLeft = e => {
+      if (myActivePage === 1){
+        setMyActivePage(myActivePage);
+      } else {
+        setMyActivePage(myActivePage - 1);
+        // setActiveImages(paginate(images, 9, myActivePage));
+      }
+    // console.log("event", e);
+    // console.log("active page:", myActivePage);
+  };
 
   useEffect(() => {
-    axios
-      .get("https://art-portfolio-be.herokuapp.com/api/posts")
-      .then(data => {
-        console.log(data);
-        setImages(paginate(data.data, 9, activePage))
-        // setImages(data.data.slice(9,18));
-      });
-  }, [activePage]);
+    axios.get("https://art-portfolio-be.herokuapp.com/api/posts").then(data => {
+      // console.log(data);
+      // setImages(paginate(data.data, 9, activePage))
+      setImages(data.data);
+      setActiveImages(paginate(data.data, 9, myActivePage));
+    });
+  }, []);
+  useEffect(()=>{
+    setActiveImages(paginate(images, 9, myActivePage));
+
+  }, [myActivePage])
 
   return (
     <Container fluid>
@@ -37,7 +63,7 @@ function Gallery() {
         Gallery
       </Header>
       <Container style={galleryContainer}>
-        {images.map(image => (
+        {activeImages.map(image => (
           <div>
             <Modal
               basic
@@ -47,31 +73,62 @@ function Gallery() {
             >
               {/* <Modal.Header>Select a Photo</Modal.Header> */}
               <Image style={{ margin: "0px 0px" }} src={image.url} />
-              <Modal.Content style={{backgroundColor:"#D3D4DE"}}>
-                <div style={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
-                      <Header as="h5" compact style={{margin:"0px", color:"#006FBB"}}>@{image.username}</Header> 
-                      {/* TODO: Make above header a link to profile route */}
-                      <Header as="h5" style={{margin:"0px", lineHeight:"20px", color:"#000639"}}>{Date(image.createdAt).toString().slice(3,10)+','+Date(image.createdAt).toString().slice(10,15)}</Header>
+              <Modal.Content style={{ backgroundColor: "#D3D4DE" }}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center"
+                  }}
+                >
+                  <Header
+                    as="h5"
+                    compact
+                    style={{ margin: "0px", color: "#006FBB" }}
+                  >
+                    @{image.username}
+                  </Header>
+                  {/* TODO: Make above header a link to profile route */}
+                  <Header
+                    as="h5"
+                    style={{
+                      margin: "0px",
+                      lineHeight: "20px",
+                      color: "#000639"
+                    }}
+                  >
+                    {Date(image.createdAt)
+                      .toString()
+                      .slice(3, 10) +
+                      "," +
+                      Date(image.createdAt)
+                        .toString()
+                        .slice(10, 15)}
+                  </Header>
                 </div>
-                  <Modal.Description>
-                      <p style={{color:"black"}}>{image.description}</p>
-                  </Modal.Description>
+                <Modal.Description>
+                  <p style={{ color: "black" }}>{image.description}</p>
+                </Modal.Description>
               </Modal.Content>
             </Modal>
           </div>
         ))}
       </Container>
-      <Pagination 
-        boundaryRange={0}
-        defaultActivePage={1}
-        activePage={activePage}
-        onPageChange={handlePageChange}
-        ellipsisItem={null}
-        firstItem={null}
-        lastItem={null}
-        siblingRange={1}
-        totalPages={10}
-        />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Button compact onClick={handlePageChangeLeft}>
+          <Icon name="chevron left" />
+        </Button>
+        <div style={{ padding: "0px 20px" }}>{myActivePage}</div>
+        <Button compact onClick={handlePageChangeRight}>
+          <Icon name="chevron right" />
+        </Button>
+      </div>
     </Container>
   );
 }
@@ -94,8 +151,8 @@ const modalStyle = {
   width: "auto",
   height: "auto",
   maxWidth: "1000px",
-  objectFit: "contain",
-//   borderRadius: "0px"
+  objectFit: "contain"
+  //   borderRadius: "0px"
 };
 
 export default Gallery;
